adb7da8aace6716c57c9feaab86d2eba8acd9d32
kc0506 <kchong0506@gmail.com>
2024-10-27 03:08:48 +0800

    update serve

--- pharmq/__main__.py
+++ pharmq/__main__.py
@@ -1,4 +1,4 @@
 from textual_serve.server import Server
 
-Server("uv run -m pharmq.app").serve()
-# Server("python3 -m pharmq.app", host="0.0.0.0", port=7414).serve()
+# Server("uv run -m pharmq.app").serve()
+Server("python3 -m pharmq.app", host="0.0.0.0", port=7414).serve()
