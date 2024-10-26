a2f976357a011fdd2ca2b9dff97abe73b8380056
kc0506 <kchong0506@gmail.com>
2024-10-27 03:09:56 +0800

    update serve

--- pharmq/__main__.py
+++ pharmq/__main__.py
@@ -1,4 +1,9 @@
 from textual_serve.server import Server
 
 # Server("uv run -m pharmq.app").serve()
-Server("python3 -m pharmq.app", host="0.0.0.0", port=7414).serve()
+Server(
+    "python3 -m pharmq.app",
+    host="0.0.0.0",
+    port=7414,
+    public_url="https://pharmq.onrender.com/",
+).serve()
